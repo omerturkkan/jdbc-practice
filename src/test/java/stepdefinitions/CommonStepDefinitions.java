556e02bf036925b031f955277bdb396276cfc9c7
omer <omer.turkkan@outlook.com>
2026-09-03 20:07:03 +0200

    refactor: move update and verify steps to CommonStepDefinitions

--- src/test/java/stepdefinitions/CommonStepDefinitions.java
+++ src/test/java/stepdefinitions/CommonStepDefinitions.java
@@ -2,7 +2,11 @@ package stepdefinitions;
 
 import io.cucumber.java.en.*;
 import manage_queries.LoantechQueries;
+import org.testng.Assert;
 import utilities.DatabaseUtils;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class CommonStepDefinitions {
 
@@ -21,5 +25,23 @@ public class CommonStepDefinitions {
     @Then("the user closes the database connection")
     public void the_user_closes_the_database_connection() {
         DatabaseUtils.closeConnection();
+    }
+
+    @When("the user updates {string} to {string} in {string} table where {string} is {string}")
+    public void the_user_updates_to_in_table_where_is(String column, String value, String table, String conditionColumn, String conditionValue) {
+        String query = LoantechQueries.update(table, column, value, conditionColumn, conditionValue);
+        DatabaseUtils.executeUpdate(query);
+    }
+
+    @Then("the {string} in {string} table where {string} is {string} should be {string}")
+    public void the_in_table_where_is_should_be(String column, String table, String conditionColumn, String conditionValue, String expectedValue) throws SQLException {
+        String selectQuery = "SELECT " + column + " FROM " + table + " WHERE " + conditionColumn + "='" + conditionValue + "'";
+        ResultSet resultSet = DatabaseUtils.executeQuery(selectQuery);
+
+        resultSet.first();
+
+        String actualValue = resultSet.getString(column);
+
+        Assert.assertEquals(actualValue, expectedValue);
     }
 }
